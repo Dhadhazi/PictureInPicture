20326cbb2006981d7376e4e6c64eb4458e7b7f09
Dhadhazi <dhadhazi@gmail.com>
2021-01-04 20:45:13 +0100

    Project complete

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Apple");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => videoElement.play();
+  } catch (e) {
+    console.log("selectMediaStream error: ", e);
+  }
+}
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+selectMediaStream();
